2ffd149ba4cf90bdf8b0a60ffce678cedf393f80
Igor <devspectre@yandex.com>
2019-04-10 20:15:00 +0300

    New widget similar to <a> tag in html

---
+++ clickablelabel.py
@@ -0,0 +1,24 @@
+import sys
+from PyQt5.QtWidgets import QLabel
+from PyQt5.QtCore import pyqtSignal, pyqtSlot
+
+class ClickableLabel(QLabel):
+	"""A widget derived from QLabel for category or filter caption and is clickable"""
+	clicked = pyqtSignal()
+	def __init__(self, text = "", parent = None):
+		QLabel.__init__(self, text, parent)
+		self.pressed = False
+		self.setObjectName("#Caption")
+		self.setStyleSheet("""#Caption{color: #ff8c00; background-color: #e7e7e7}
+			#Caption:hover{background-color: #d7d7d7}""")
+
+	#override mousePressEvent
+	def mousePressEvent(self, event):
+		QLabel.mousePressEvent(self, event)
+		self.pressed = True
+
+	#override mouseReleaseEvent
+	def mouseReleaseEvent(self, event):
+		QLabel.mouseReleaseEvent(self, event)
+		if self.pressed:
+			self.clicked.emit()
